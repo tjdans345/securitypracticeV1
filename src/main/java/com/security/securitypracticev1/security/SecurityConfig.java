7cd8891c4bd77e58c2320a11c1b7c3b1c7ee1ace
tjdans34568 <tjdans34568@gmail.com>
2021-11-24 15:29:54 +0900

    Initial commit

---
+++ src/main/java/com/security/securitypracticev1/security/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.security.securitypracticev1.security;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    /**
+     * 실제 인증을 진행할 Provider
+     * @param auth
+     * @throws Exception
+     */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
+
+    /**
+     * 이미지 , 자바스크립트 , css 디렉토리 보안 설정
+     * @param web
+     * @throws Exception
+     */
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+    }
+
+
+}
---
+++ src/test/java/com/security/securitypracticev1/SecuritypracticeV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.securitypracticev1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritypracticeV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
